926326429ceb57f0d393a80faf5c321269400a4e
Noah Sun <sundong95125@gmail.com>
2017-02-20 17:10:30 -0500

    First commit

---
+++ app/src/main/java/com/dsun2wpi/snap/Passenger.java
@@ -0,0 +1,13 @@
+package com.dsun2wpi.snap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Passenger extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passenger);
+    }
+}
